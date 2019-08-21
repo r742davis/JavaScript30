faabac40bb2d524343985fb5ecd76341e191b902
Richard <richard.trow.davis@gmail.com>
2019-08-21 13:09:13 -0400

    Finished Day 19 - Webcam Fun

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,68 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(localMediaStream => {
+    console.log(localMediaStream);
+    video.srcObject = localMediaStream;
+    video.play();
+    })
+    .catch(err => {
+      console.log("Oh No!", err)
+    })
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height)
+    let pixels = ctx.getImageData(0, 0, width, height)
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.5;
+    ctx.putImageData(pixels, 0, 0);
+
+  }, 16)
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  //take data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome man" />`;
+  link.textContent = 'Download Image';
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100 //red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50  //green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5  //blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0] //red
+    pixels.data[i + 100] = pixels.data[i + 1] //green
+    pixels.data[i - 350] = pixels.data[i + 2] //blue
+  }
+  return pixels;
+}
+
+getVideo();
+
+
+video.addEventListener('canplay', paintToCanvas)
